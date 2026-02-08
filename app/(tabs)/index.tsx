80297577df11acab8fdab2a6558ac9775b87692a
Stefan Predolac <stefan.predolac@appsco.com>
2026-02-08 19:30:37 +0100

    api for fetching tmdb movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -13,7 +16,10 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <SearchBar />
+        <SearchBar
+          onPress={() => router.push("/search")}
+          placeholder="Search for a movie"
+        />
       </ScrollView>
     </View>
   );
--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,7 +1,12 @@
 import { icons } from "@/constants/icons";
-import { Image, View } from "react-native";
+import { Image, TextInput, View } from "react-native";
 
-const SearchBar = () => {
+interface Props {
+  onPress?: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
@@ -9,6 +14,14 @@ const SearchBar = () => {
         className="size-5"
         resizeMode="contain"
         tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        className="flex-1 ml-2 text-white"
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
       />
     </View>
   );
