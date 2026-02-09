3883000712f6aefeec2199c4480e21c2bc28ae12
Stefan Predolac <stefan.predolac@appsco.com>
2026-02-09 10:13:02 +0100

    Added Flatlist for showing movies

--- services/api.ts
+++ services/api.ts
@@ -7,7 +7,7 @@ export const TMDB_CONFIG = {
   },
 };
 
-const fetchMovies = async ({ query }: { query: string }) => {
+export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
@@ -24,17 +24,3 @@ const fetchMovies = async ({ query }: { query: string }) => {
   const data = await response.json();
   return data.results;
 };
-
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjdjZTliMTlhZTI4ZGViZjMwZWI1MGY1YzY2N2IwMCIsIm5iZiI6MTYyOTA5NzI4MS42NDIsInN1YiI6IjYxMWEwZDQxNmMxOWVhMDA0NjRiMjg5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dKVwdiUJpIJcxDxKuuNRLN7bcwde1VAWzACIySB3h4I'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
